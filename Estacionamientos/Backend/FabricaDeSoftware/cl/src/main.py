dddedb784201a247217798fbc607c1fdd0a1b6df
Tiago Huber <thuber2018@udec.cl>
2023-10-03 20:48:51 -0300

    funciona main

--- Estacionamientos/Backend/FabricaDeSoftware/cl/src/main.py
+++ Estacionamientos/Backend/FabricaDeSoftware/cl/src/main.py
@@ -28,7 +28,7 @@ img_counter=0
 
 
 
-USED_SPOTS = count_vehicles("detectar/1ddb6186-351a-46f7-bcf3-4e6466d5b2a6.jpeg")
+#USED_SPOTS = count_vehicles('detectar/1ddb6186-351a-46f7-bcf3-4e6466d5b2a6.jpeg')
 
 
 class ConnectionManager:
